179959646e984d0c827e37f69caa095f29e57da0
Allan Coutarel <contact@allancoutarel.fr>
2023-02-25 14:09:47 +0100

    Add check syntax

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from utils.utils import *
+import sys
 
 app = Flask(__name__)
 
@@ -46,4 +47,11 @@ def loginRoute():
     return '''<h1>LOGIN</h1>'''
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1 :
+        if sys.argv[1] == "check_syntax":
+            print("Build [ OK ]")
+            exit(0)
+        else:
+            print("Passed argument not supported ! Supported argument : check_syntax")
+            exit(1)
     app.run(debug=True)
