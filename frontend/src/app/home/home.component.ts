87ba262157ca3971465718066ec0c363b60c286f
Naofel EL ALOUANI <naofel.eal@gmail.com>
2023-03-10 10:23:04 +0100

    profile component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Bula } from '../shared/models/Bula.model';
 import { HttpClient } from '@angular/common/http';
 import { ApiConstantsService } from '../shared/constants/api-constants.service';
-import { HomeService } from '../shared/dependencies/home.service';
+import { BulaService } from '../shared/dependencies/bula.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,28 +11,15 @@ import { HomeService } from '../shared/dependencies/home.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit{
-  constructor(private http: HttpClient, private apiConstantsService: ApiConstantsService, public homeService: HomeService) { 
-    this.homeService.getBulas().subscribe((bulas) => {
+  public bulas: Bula[];
+
+  constructor(public bulaService: BulaService) { 
+    this.bulaService.getAllBulas().subscribe((bulas) => {
       this.bulas = bulas;
     });
   }
-  
-  public bulas: Bula[] = []
-  public isCreateBulaVisible = false;
-  public isTopicsVisible = false;
 
   ngOnInit(): void {
-    this.homeService.loadAllBulas();
-  }
-
-  public close_create_bula(isBulaPosted: boolean) {
-    this.isCreateBulaVisible = false;
-    if(isBulaPosted) {
-      this.homeService.loadAllBulas();
-    }
-  }
-
-  public switch_create_bula_visibility() {
-    this.isCreateBulaVisible = !this.isCreateBulaVisible;
+    this.bulaService.loadAllBulas();
   }
 }
--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Bula } from '../shared/models/Bula.model';
+import { BulaService } from '../shared/dependencies/bula.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  public userId: string;
+  public bulas: Bula[] = []
 
+  constructor(public bulaService: BulaService, private route: ActivatedRoute) { 
+    this.route.paramMap.subscribe((params) => {
+        this.userId = "" + params.get("userId")
+      }
+    );
+    this.bulaService.getUserBulas().subscribe((bulas) => {
+      this.bulas = bulas;
+    });
+  }
+
+  ngOnInit(): void {
+    this.bulaService.loadUserBulas(this.userId);
+    console.log(this.userId)
+  }
 }
